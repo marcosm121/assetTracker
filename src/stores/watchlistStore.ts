dee1f64b95056cef4ca3adce379379dff71870c6
Marcos <martinezmarcos121@gmail.com>
2026-03-16 13:30:06 -0300

    feat: simplify watchlistStore to string[], update preferencesStore

--- src/stores/watchlistStore.ts
+++ src/stores/watchlistStore.ts
@@ -1,29 +1,15 @@
 import { create } from 'zustand'
 import { loadWatchlist, saveWatchlist } from '../utils/storage'
-import type { WatchlistItem } from '../adapters/types'
 
 interface WatchlistState {
-  items: WatchlistItem[]
-  addItem: (item: WatchlistItem) => void
-  removeItem: (symbol: string, market: string) => void
-  hasItem: (symbol: string, market: string) => boolean
+  items: string[]
+  setItems: (items: string[]) => void
 }
 
-export const useWatchlistStore = create<WatchlistState>((set, get) => ({
+export const useWatchlistStore = create<WatchlistState>((set) => ({
   items: loadWatchlist(),
-
-  addItem: (item) => {
-    const items = [...get().items, item]
+  setItems: (items) => {
     saveWatchlist(items)
     set({ items })
   },
-
-  removeItem: (symbol, market) => {
-    const items = get().items.filter(i => !(i.symbol === symbol && i.market === market))
-    saveWatchlist(items)
-    set({ items })
-  },
-
-  hasItem: (symbol, market) =>
-    get().items.some(i => i.symbol === symbol && i.market === market),
 }))
